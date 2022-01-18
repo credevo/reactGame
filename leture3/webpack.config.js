5eeb0a01ee007b588c7d5b542df2c5a77e7ef6d5
credevo <credevo@gmail.com>
2022-01-18 18:29:26 +0900

    react webpack setting init

---
+++ leture3/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+    name : 'word-relay-setting',
+    mode : 'development', //실서비스 : production
+    devtool : 'eval', //빠르게
+    
+    entry : {
+        app : ['./client'],
+    },//입력
+    resolve : {
+        extensions : ['.js','.jsx'],
+    },    
+    output : {
+        path : path.join(__dirname,'dist'),
+        filename : 'app.js'
+    },//출력
+    
+    module :{
+        rules: [
+        {
+          test: /\.jsx$/,
+          loader : 'babel-loader' ,
+          options : {
+            presets: ['@babel/preset-env','@babel/preset-react'],
+            
+          }
+        }
+      ]
+    },
+    // plugins :{}
+    // module : {},
+    // plugins : [],
+}
