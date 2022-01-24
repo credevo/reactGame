34e38112bd51a6087efe9d9a25315646bd82b560
credevo <creator.oh@gmail.com>
2022-01-24 09:16:36 +0900

    baseball : class component complete..

--- baseball/BaseBall.jsx
+++ baseball/BaseBall.jsx
@@ -3,7 +3,14 @@ import Try from './Try';
 
 //숫자 4개를 겹치지 않고 랜덤하게 뽑는 함수
 function getNumbers(){
-    return 1234;
+    const candidates = [1,2,3,4,5,6,7,8,9];
+    const array=[];
+    for(let i=0; i<4; i++){
+        const chose = candidates.splice(Math.floor(Math.random()*(9-i)),1)[0];
+        array.push(chose);
+    }
+    console.log('답은 : ', array.join(''));
+    return array;
 }
 
 class BaseBall extends Component{
@@ -26,42 +33,75 @@ class BaseBall extends Component{
         result : '',
         value : '',
         answer : getNumbers(),
-        strike : 0,
-        ball : 0,
         tries :[]
     }
-    onSubmitForm = ()=>{
-
+    onSubmitForm = (e)=>{
+        e.preventDefault();
+        if(this.state.value === this.state.answer.join('')){
+            this.setState({
+                /**
+                 * 
+                 * react
+                 *참조된 주소 자체를 변경해서 데이타가 바뀌는거 감지하여 rendering 
+                 * 
+                 */
+                tries : [...this.state.tries,{try :  this.state.value, result :'홈런'}]
+            })
+            alert('게임을 다시 시작합니다.!')
+            this.setState({
+                answer : getNumbers(),
+                value : '',
+                tries : [],
+            });
+        }else{
+            const valueArray = this.state.value.split('').map(v=>parseInt(v));
+            let strike = 0;
+            let ball = 0;
+            if(this.state.tries.length >= 9){
+                this.setState({
+                    result : `10번 틀려서 실패! 답은  ${this.state.answer.join(',')}`
+                });
+                alert('게임을 다시 시작합니다')
+                this.setState({
+                    answer : getNumbers(),
+                    value : '',
+                    tries : [],
+                });
+            }else{
+                for(let i=0; i<4; i++){
+                    if(valueArray[i] === this.state.answer[i]){
+                        strike += 1;
+                    }else if(this.state.answer.includes(valueArray[i])){
+                        ball += 1;
+                    }
+                }
+                this.setState({
+                    tries : [...this.state.tries,{try: this.state.value, result : `strkie : ${strike}, ball : ${ball}`}],
+                })
+            }
+        }
     }
-    onChangeInput = ()=>{
-
+    onChangeInput = (e)=>{
+        this.setState({
+            value : e.target.value,
+        })
     }
-    fruits = [
-        {fruit :'사과', taste : '맛있다1'},
-        {fruit :'바나나', taste : '맛있다2'},
-        {fruit :'포도', taste : '맛있다3'},
-        {fruit :'귤', taste : '맛있다4'},
-        {fruit :'감', taste : '맛있다5'},
-        {fruit :'배', taste : '맛있다6'},
-        {fruit :'밤', taste : '맛있다7'},
-        {fruit :'사과', taste : '맛있다8'},
-        {fruit :'사과', taste : '맛있다8'},
-    ];
+    
     render = ()=>{
-        
+        const {result, value, tries} = this.state; // 구조 분해를 통해 state 간략
         return(
             <>
                 <h1>{this.state.result}</h1>
                 <form onSubmit={this.onSubmitForm}>
-                    <input maxLength={4} value={this.value} onChange={this.onChangeInput} />
+                    <input maxLength={4} value={this.state.value} onChange={this.onChangeInput} />
                     <button>입력</button>
                 </form>
                 <div>시도 : {this.state.tries.length}</div>
                 <ul>
                     {
-                        this.fruits.map((v,i)=>{
+                        this.state.tries.map((v,i)=>{
                             return (
-                                <Try key={v+'0'+i} value={v} index={i}></Try>
+                                <Try key={v+'0'+i} tryInfo={v} index={i}></Try>
                             );
                         })
                     }
--- baseball/Try.jsx
+++ baseball/Try.jsx
@@ -1,25 +1,25 @@
 import React, {Component} from 'react';
 // const {useRef, useState, Fragment} = React;
 
-// class Try extends Component {
-//      render(){
-//             return (
-//                 <li>
-//                     {this.props.value.fruit}-{this.props.value.taste}-[{this.props.index}]
-//                 </li>
-//             );   
-//      }
-// } 
-const Try = (props)=>{
-    const value = props.value; 
-    const index = props.index;
-    return (
+class Try extends Component {
+     
+    render = ()=>(
         <li>
-            {value.fruit}-{value.taste}-[{index}] 
+            {this.props.tryInfo.try}-{this.props.tryInfo.result}-[{this.props.index}]
         </li>
-    );
+    );   
+     
+} 
+// const Try = (props)=>{
+//     const tryInfo = props.tryInfo; 
+//     const index = props.index;
+//     return (
+//         <li>
+//             {tryInfo.try}-{tryInfo.result}-[{index}] 
+//         </li>
+//     );
     
-};
+// };
     
 
 
